6b34ecc0449fbe67b5a6507e5015613daa477b48
Pedro Tigre <pedrogtigre@gmail.com>
2022-07-29 15:33:04 -0300

    Comment added

--- pages/cat-fact.js
+++ pages/cat-fact.js
@@ -1,5 +1,6 @@
 // import { useState, useEffect } from 'react';
 import useSWR from 'swr';
+// Client data fetching can be used with getStaticProps, this way we can preload some data (e.g. preload the main events with getStaticProps) and update it (with client fetching). This aproach doesn't makes sense with ServerSideRendering (since it regenerates every request).
 
 function catFact() {
   // const [data, setData] = useState();
--- pages/user-profile.js
+++ pages/user-profile.js
@@ -6,8 +6,8 @@ export async function getServerSideProps(context) {
   const { req, res } = context;
 
   // Server side code
-  console.log(req);
-  console.log(res);
+  // console.log(req);
+  // console.log(res);
 
   // No need to getStaticPaths, since it doesn't pre-generate pages. Give us access to req and res. And makes a new request every time.
 
